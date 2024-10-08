9b9b0444afbdb72072beb5925f9004a8d28d5518
Reringuy <jounheringer@outlook.com>
2024-10-07 20:33:39 -0400

    Fixed table list

--- frontAngular17/src/app/student/student-list/student-list.component.ts
+++ frontAngular17/src/app/student/student-list/student-list.component.ts
@@ -3,6 +3,8 @@ import { Student } from '../../../shared/models/student';
 import { StudentListItemComponent } from "../student-list-item/student-list-item.component";
 import { FormsModule } from '@angular/forms';
 import {NgForOf} from "@angular/common";
+import {ApiService} from "../../../shared/services/api.service";
+import {EventService} from "../../../shared/services/EventService";
 
 @Component({
   selector: 'app-student-list',
@@ -16,5 +18,16 @@ export class StudentListComponent {
 
   toogleStudent(student: Student) {
     student.aproved = !student.aproved;
+  }
+
+  constructor(private api: ApiService, private events: EventService) { }
+
+  removeStudent(student: Student) {
+    this.events.emit('removeStudent', student.id);
+    this.api.removeStudent(student.id).subscribe()
+  }
+
+  toogleApproved(student: Student){
+    student.aproved = !student.aproved
   }
 }
